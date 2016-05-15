312de44f5d6aead2c89c12888b047c70e7ed3447
bharding2 <b.harding@gmail.com>
2016-05-15 16:11:00 -0700

    iterating through projects

--- app/js/entry.jsx
+++ app/js/entry.jsx
@@ -1,82 +1,37 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const superAgent = require('superAgent');
 
-var HelloPortfolio = React.createClass({
-  render: function() {
-    return (
-      <p>
-        Hello, <input type="text" placeholder="Your name here" />!
-        It is { this.props.date.toTimeString() }
-      </p>
-    );
-  }
-});
-
-setInterval(function() {
-  ReactDOM.render(
-    <HelloPortfolio date = { new Date() } />,
-    document.getElementById('example')
-  );
-}, 500);
-
-var root = React.DOM.ul({ varName: 'my-list' },
-  React.DOM.li(null, 'First list item'),
-  React.DOM.li(null, 'Second list item')
-);
-
-ReactDOM.render(root, document.getElementById('list1'));
-
-var LikeButton = React.createClass({
-  getInitialState: function() {
-    return { liked: false };
-  },
-  handleClick: function(event) {
-    this.setState({ liked: !this.state.liked });
-  },
-  render: function() {
-    var text = this.state.liked ? 'like' : 'haven\'t liked';
-    return (
-      <p onClick={ this.handleClick}>
-        You { text } this. Click to toggle.
-      </p>
-    );
-  }
-});
-
-ReactDOM.render(
-  <LikeButton />,
-  document.getElementById('list2')
-);
-
-var Avatar = React.createClass({
+const Projects = React.createClass({
   render: function() {
     return (
       <div>
-        <PageLink pagename={ this.props.pagename } />
-      </div>
+        { this.props.data.map(function(project) {
+          return (
+            <ul key={ project.name }>
+              <li>{ project.name }</li>
+              <li>{ project.title }</li>
+              <li>{ project.description }</li>
+              <li>{ project.publishDate }</li>
+              <li>{ project.siteURL }</li>
+              <li>{ project.repoURL }</li>
+              <li>{ project.thumbnailURL }</li>
+              <li>{ project.categories.join(', ') }</li>
+             </ul>
+           )
+         })
+        }
+       </div>
     );
   }
 });
 
-var PagePic = React.createClass({
-  render: function() {
-    return(
-      <img src={ 'https://graph.facebook.com/' + this.props.pagename + '/picture' } />
-    );
-  }
+superAgent
+.get('http://localhost:5555/api/projects')
+.end((err, superRes) => {
+  var projects = JSON.parse(superRes.text);
+  ReactDOM.render(
+    <Projects data={ projects } />,
+    document.getElementById('projects')
+  );
 });
-
-var PageLink = React.createClass({
-  render: function() {
-    return (
-      <a href={ 'https://www.facebook.com/' + this.props.pagename }>
-        <PagePic pagename={ this.props.pagename } />
-      </a>
-    );
-  }
-});
-
-ReactDOM.render(
-  <Avatar pagename="Engineering" />,
-  document.getElementById('picture-thing')
-);
