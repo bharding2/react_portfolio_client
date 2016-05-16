dd7d52134b0c78bd8fbe99e15bdcf7f22c8286e7
bharding2 <b.harding@gmail.com>
2016-05-16 12:10:20 -0700

    edit and delete

--- app/js/entry.jsx
+++ app/js/entry.jsx
@@ -11,10 +11,10 @@ const ProjectBox = React.createClass({
       this.setState({ data: superData });
     });
   },
-  handleProjectSubmit: function(comment) {
+  handleProjectSubmit: function(project) {
     superAgent
     .post(this.props.url)
-    .send(comment)
+    .send(project)
     .end((err, superRes) => {
       this.loadProjectsFromServer();
     });
@@ -28,7 +28,7 @@ const ProjectBox = React.createClass({
   render: function() {
     return (
       <div>
-        <ProjectList data={ this.state.data } />
+        <ProjectList data={ this.state.data } url={ this.props.url } loadProjectsFromServer = { this.loadProjectsFromServer }/>
         <ProjectForm onProjectSubmit={ this.handleProjectSubmit } />
       </div>
     );
@@ -37,6 +37,8 @@ const ProjectBox = React.createClass({
 
 const ProjectList = React.createClass({
   render: function() {
+    var url = this.props.url;
+    var loadProjectsFromServer = this.props.loadProjectsFromServer
     var projectNodes = this.props.data.map(function(project) {
       return (
         <Project
@@ -49,6 +51,8 @@ const ProjectList = React.createClass({
           repoURL={ project.repoURL }
           thumbnailURL={ project.thumbnailURL }
           categories={ project.categories }
+          url={ url }
+          loadProjectsFromServer={ loadProjectsFromServer }
         />
       );
     });
@@ -196,7 +200,136 @@ const ProjectForm = React.createClass({
   }
 });
 
+const ProjectEditForm = React.createClass({
+  getInitialState: function() {
+    return {
+      title: '',
+      description: '',
+      publishDate: '',
+      siteURL: '',
+      repoURL: '',
+      thumbnailURL: '',
+      categories: ''
+    };
+  },
+  handleTitleChange: function(e) {
+    this.setState({ title: e.target.value });
+  },
+  handleDescriptionChange: function(e) {
+    this.setState({ description: e.target.value });
+  },
+  handlePublishDateChange: function(e) {
+    this.setState({ publishDate: e.target.value });
+  },
+  handleSiteURLChange: function(e) {
+    this.setState({ siteURL: e.target.value });
+  },
+  handleRepoURLChange: function(e) {
+    this.setState({ repoURL: e.target.value });
+  },
+  handleThumbnailURLChange: function(e) {
+    this.setState({ thumbnailURL: e.target.value });
+  },
+  handleCategoriesChange: function(e) {
+    this.setState({ categories: e.target.value });
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = this.state.title.trim() || this.props.title;
+    var description = this.state.description.trim() || this.props.description;
+    var publishDate = this.state.publishDate.trim() || this.props.publishDate;
+    var siteURL = this.state.siteURL.trim() || this.props.siteURL;
+    var repoURL = this.state.repoURL.trim() || this.props.repoURL;
+    var thumbnailURL = this.state.thumbnailURL.trim() || this.props.thumbnailURL;
+    var categories = this.state.categories.trim() || this.props.categories;
+
+    this.props.onProjectSubmit({
+      name: this.props.name,
+      title: title,
+      description: description,
+      publishDate: publishDate,
+      siteURL: siteURL,
+      repoURL: repoURL,
+      thumbnailURL: thumbnailURL,
+      categories: categories
+    });
+    this.setState({
+      title: '',
+      description: '',
+      publishDate: '',
+      siteURL: '',
+      repoURL: '',
+      thumbnailURL: '',
+      categories: ''
+    });
+  },
+  render: function() {
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type="text"
+          placeholder="title"
+          value={ this.state.title }
+          onChange={ this.handleTitleChange }
+        />
+        <input
+          type="text"
+          placeholder="description"
+          value={ this.state.description }
+          onChange={ this.handleDescriptionChange }
+        />
+        <input
+          type="text"
+          placeholder="publishDate"
+          value={ this.state.publishDate }
+          onChange={ this.handlePublishDateChange }
+        />
+        <input
+          type="text"
+          placeholder="siteURL"
+          value={ this.state.siteURL }
+          onChange={ this.handleSiteURLChange }
+        />
+        <input
+          type="text"
+          placeholder="repoURL"
+          value={ this.state.repoURL }
+          onChange={ this.handleRepoURLChange }
+        />
+        <input
+          type="text"
+          placeholder="thumbnailURL"
+          value={ this.state.thumbnailURL }
+          onChange={ this.handleThumbnailURLChange }
+        />
+        <input
+          type="text"
+          placeholder="categories"
+          value={ this.state.categories }
+          onChange={ this.handleCategoriesChange }
+        />
+        <button type="submit">Edit</button>
+      </form>
+    );
+  }
+});
+
 const Project = React.createClass({
+  handleProjectDelete: function(project) {
+    superAgent
+    .delete(this.props.url + '/' + this.props.name)
+    .end((err, superRes) => {
+      this.props.loadProjectsFromServer();
+    });
+  },
+  handleProjectEdit: function(project) {
+    superAgent
+    .put(this.props.url + '/' + this.props.name)
+    .send(project)
+    .end((err, superRes) => {
+      this.props.loadProjectsFromServer();
+    });
+  },
   render: function() {
     return (
       <ul key={ this.props.name }>
@@ -208,6 +341,17 @@ const Project = React.createClass({
         <li>{ this.props.repoURL }</li>
         <li>{ this.props.thumbnailURL }</li>
         <li>{ this.props.categories }</li>
+        <li><ProjectEditForm
+          onProjectSubmit={ this.handleProjectEdit }
+          name={ this.props.name }
+          title={ this.props.title }
+          description={ this.props.description }
+          publishDate={ this.props.publishDate }
+          siteURL={ this.props.siteURL }
+          repoURL={ this.props.repoURL }
+          thumbnailURL={ this.props.thumbnailURL }
+          categories={ this.props.categories }
+        /><button onClick={ this.handleProjectDelete }>Delete</button></li>
       </ul>
     );
   }
